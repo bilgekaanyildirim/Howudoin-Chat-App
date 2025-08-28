e0846ee16688835b3dab69c184808451d9025277
Karanaka <yildirimbilgekaan03@gmail.com>
2025-08-28 09:10:54 +0300

    Project initialized

---
+++ backend/src/main/java/com/howudoin/backend/HowudoinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.howudoin.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowudoinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowudoinBackendApplication.class, args);
+	}
+
+}
